554992c0699484d5c9f7a3a571cc59c1036678fc
Bernize Eugene <32181589+BernizeEugene@users.noreply.github.com>
2017-10-14 16:02:47 -0700

    adjusted web pages

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation();
+
+var today = new Date();
+var hourNow= today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+  greeting='Good Evening!';
+} else if (hourNow > 12) { greeting='Good Afternoon!';
+} else {
+  greeting='Welcome!';
+}
+
+document.write('<h2>' + greeting + '</h2>');
